8b66359e6c1c77d719ebd0a89ecb5b10eb49732a
Julie Ganeshan <sangan@live.com>
2023-03-13 18:19:39 -0400

    oops

--- gptsearch/pdf_to_text.py
+++ gptsearch/pdf_to_text.py
@@ -27,7 +27,7 @@ def main() -> None:
     parser.add_argument("library", type=Path)
     parser.add_argument("-o", "--output", type=lambda s: str(Path(s).resolve()), default=None)
     args = parser.parse_args()
-    to_convert = args.file.resolve(strict=True)
+    to_convert = args.library.resolve(strict=True)
     print(convert_library(to_convert, args.output))
 
 if __name__ == "__main__":
